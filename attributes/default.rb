8cbdb9e8e6a63667c24aca5ae7dcad36ff078ea7
Michael Nikitochkin <miry.sof@gmail.com>
2014-11-29 01:23:01 +0100

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default[:nginx][:version]           = "1.4.4"
+default[:nginx][:version]           = "1.6.2"
 default[:nginx][:version]           = "1.4.1" if node[:platform_version].to_f < 12.04
 default[:nginx][:apt_packages]      = %w[nginx-common nginx-full nginx]
 default[:nginx][:apt_keyserver]     = "keyserver.ubuntu.com"
